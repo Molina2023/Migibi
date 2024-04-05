ace433840c1a36b02c0bd3a88a188759085a5d38
nisit <nisit@ElHipopotamitoMicroscopico>
2024-04-05 14:13:58 -0600

    Inicio

---
+++ src/BackEnd/Enum.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackEnd;
+
+/**
+ *
+ * @author nisit
+ */
+public class Enum {
+    public enum Clasificacion {
+        FRUTA,
+        VEGETAL,
+        LACTEO,
+        CEREAL,
+        ESPECIE,
+        MARISCO; 
+    }
+}
